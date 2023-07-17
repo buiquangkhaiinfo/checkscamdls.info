eb7861eb08197a8f18a56146da7a2e32fed82a3a
buiquangkhaiinfo <122881045+buiquangkhaiinfo@users.noreply.github.com>
2023-07-17 08:34:09 +0700

    Add files via upload

---
+++ ThongTinThanhToan/js/config.js
@@ -0,0 +1,42 @@
+const USER = {
+    banks: [{
+            name: "MB BANK",
+            logo: "./images/mbbank.png",
+            number: "05052008567890 ",
+            owner: "BUI QUANG KHAI"
+        },
+      
+        {
+            name: "MOMO",
+            logo: "./images/momo.jpeg",
+            number: "0352876128",
+            owner: "Bùi Quang Khải",
+            fastTransfer: true
+        },
+
+        {
+            name: "THESIEURE",
+            logo: "./images/tsr1.jpg",
+            number: "quangkhai124365@gmail.com",
+            owner: "Bùi Quang Khải",
+            fastTransfer: true
+        },
+
+        {
+            name: "NAPTHE1S",
+            logo: "./images/nt1s.jpg",
+            number: "0352876128",
+            owner: "Bùi Quang Khải",
+            fastTransfer: true
+        },
+
+        {
+            name: "CARD5S",
+            logo: "./images/card5s.jpg",
+            number: "quangkhai124365@gmai.com",
+            owner: "Bùi Quang Khải",
+            fastTransfer: true
+        },
+       
+    ]
+};
